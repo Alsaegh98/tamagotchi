40899c0fef22f592a8459c3947cac6e5ff7161ae
alsaegh98 <abdullajaffer8@gmail.com>
2024-11-08 16:54:50 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,125 @@
+// 1) Define the required variables used to track the state of the game.
+
+// 2) Store cached element references.
+
+// 3) Upon loading, the game state should be initialized, and a function should 
+//    be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic. 
+
+// 6) Handle each instance of a player clicking a button with the use of a 
+//    `handleClick()` function.
+
+// 7) Create reset functionality.
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+const state = {
+    boredom : 0,
+    hunger : 0,
+    sleepiness : 0,
+};
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let timer;
+let gameover;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const boredomStatEl = document.getElementById('boredom-stat');
+const hungerStatEl = document.getElementById('hunger-stat');
+const sleepinessStatEl = document.getElementById('sleepiness-stat');
+
+const playBtnEl = document.getElementById('play');
+const feedBtnEl = document.getElementById('feed');
+const sleepBtnEl = document.getElementById('sleep');
+
+const gameMessageEl = document.getElementById('message');
+const resetBtnEl = document.getElementById('restart');
+
+
+//console.log(boredomStatEl, hungerStatEl, sleepinessStatEl, playBtnEl, feedBtnEl, sleepBtnEl, gameMessageEl, resetBtnEl);
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    console.log('Game initialized');
+    gameOver = false;
+  
+    timer = setInterval(runGame, 2000);
+  
+    render();
+  };
+  
+  const runGame = () => {
+    updateStates();
+    checkGameOver();
+    render();
+  };
+  
+  const updateStates = () => {
+    const increment = () => Math.floor(Math.random() * 4);
+    state.boredom += increment();
+    state.hunger += increment();
+    state.sleepiness += increment();
+  };
+  
+  const render = () => {
+    boredomStatEl.textContent = state.boredom;
+    hungerStatEl.textContent = state.hunger;
+    sleepinessStatEl.textContent = state.sleepiness;
+  
+    if (gameOver) {
+      clearInterval(timer);
+      gameMessageEl.classList.remove('hidden');
+      resetBtnEl.classList.remove('hidden');
+    }
+  };
+  
+  const checkGameOver = () => {
+    if (state.boredom >= 10 || state.hunger >= 10 || state.sleepiness >= 10) {
+      gameOver = true;
+    }
+  };
+  
+  const playBtnClick = () => {
+    state.boredom = 0;
+    render();
+  };
+  
+  const feedBtnClick = () => {
+    state.hunger = 0;
+    render();
+  };
+  
+  const sleepBtnClick = () => {
+    state.sleepiness = 0;
+    render();
+  };
+  
+  const resetGame = () => {
+    gameMessageEl.classList.add('hidden');
+    resetBtnEl.classList.add('hidden');
+    state.boredom = 0;
+    state.hunger = 0;
+    state.sleepiness = 0;
+    gameOver = false;
+  
+    timer = setInterval(runGame, 2000);
+  
+    render();
+  };  
+
+/*----------------------------- Event Listeners -----------------------------*/
+playBtnEl.addEventListener('click', playBtnClick);
+feedBtnEl.addEventListener('click', feedBtnClick);
+sleepBtnEl.addEventListener('click', sleepBtnClick);
+resetBtnEl.addEventListener('click', resetGame);
+
+init();
